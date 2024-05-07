fb05e8f1a5e10a4c4584e18d2235feffc0f708ed
galaxy378 <jiajunsun998@gmail.com>
2024-05-07 17:18:09 -0600

    Add quotes

---
+++ generator.js
@@ -0,0 +1,19 @@
+caoCaoQuotes = [
+  "It is better to be feared than to be loved.",
+  "Every adversity presents an opportunity for growth and learning.",
+  "The truly powerful are those who can control their own desires and impulses."
+];
+
+hitlerQuotes = [
+  "If you win, you need not have to explain...If you lose, you should not be there to explain!",
+  "Anyone can deal with victory. Only the mighty can bear defeat.",
+  "I use emotion for the many and reserve reason for the few."
+];
+
+bismarkQuotes = [
+  "Politics is the art of the possible.",
+  "The weak are strong because they are reckless. The strong are weak because they have scruples.",
+  "Only a fool learns from his own mistakes. The wise man learns from the mistakes of others."
+];
+
+
